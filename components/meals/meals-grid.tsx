6e3495489eb9fe7fe8dad7b12a2f937c8715d00b
Aman Kumar <amankumaroo784@gmail.com>
2024-09-28 13:36:58 +0530

    added meals page and its components v1

---
+++ components/meals/meals-grid.tsx
@@ -0,0 +1,30 @@
+import React, { FC } from "react";
+import MealItem from "./meal-item";
+import classes from "./meals-grid.module.css";
+
+type MealItemProps = {
+  id: number;
+  title: string;
+  slug: string;
+  image: string;
+  summary: string;
+  creator: string;
+};
+
+type MealsGridProps = {
+  meals: MealItemProps[];
+};
+
+const MealsGrid: FC<MealsGridProps> = ({ meals }) => {
+  return (
+    <ul className={classes.meals}>
+      {meals.map((meal) => (
+        <li key={meal.id}>
+          <MealItem {...meal} />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default MealsGrid;
